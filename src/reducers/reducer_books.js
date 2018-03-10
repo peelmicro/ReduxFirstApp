61d0a65155a678fa41958af82f0383fd791909d2
RIM\Juan.Pablo.Perez <juan.pablo@retailinmotion.com>
2018-03-10 10:37:06 +0000

    Demo App finished

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "JavaScript: The Good Parts", pages: 101 },
+    { title: "Harry Potter", pages: 303 },
+    { title: "The Dark Tower", pages: 321 },
+    { title: "Eloquent Ruby", pages: 541 }
+  ]
+}
